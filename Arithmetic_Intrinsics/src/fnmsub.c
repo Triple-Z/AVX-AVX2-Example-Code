59850ede8824da5d552a4f689b433ab24a329a5c
TripleZ <me@triplez.cn>
2018-08-20 21:57:51 +0800

    ADD fnmadd, fnmsub
    - UPDATE README

---
+++ Arithmetic_Intrinsics/src/fnmsub.c
@@ -0,0 +1,64 @@
+/**
+ * Author: TripleZ<me@triplez.cn>
+ * Date: 2018-08-20
+ */
+
+#include <immintrin.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    
+    // Single-precision multiply negated and add with 128-bit vectors (FMA)
+    __m128 float_128_vec_0 = _mm_set1_ps(8.0);
+    __m128 float_128_vec_1 = _mm_set1_ps(20.0);
+    __m128 float_128_vec_2 = _mm_set1_ps(2.0);
+
+    __m128 float_128_result = _mm_fnmsub_ps(float_128_vec_0, float_128_vec_1, float_128_vec_2);
+
+    float* flo = (float*) &float_128_result;
+    printf("float:\t\t%f, %f, %f, %f\n", flo[0], flo[1], flo[2], flo[3]);
+
+    // Double-precision multiply negated and add with 128-bit vectors (FMA)
+    __m128d double_128_vec_0 = _mm_set1_pd(8.0);
+    __m128d double_128_vec_1 = _mm_set1_pd(20.0);
+    __m128d double_128_vec_2 = _mm_set1_pd(2.0);
+
+    __m128d double_128_result = _mm_fnmsub_pd(double_128_vec_0, double_128_vec_1, double_128_vec_2);
+
+    double* dou = (double*) &double_128_result;
+    printf("double:\t\t%lf, %lf\n", dou[0], dou[1]);
+
+    // Single-precision multiply negated and add with 256-bit vectors (FMA)
+    __m256 float_256_vec_0 = _mm256_set1_ps(8.0);
+    __m256 float_256_vec_1 = _mm256_set1_ps(20.0);
+    __m256 float_256_vec_2 = _mm256_set1_ps(2.0);
+
+    __m256 float_256_result = _mm256_fnmsub_ps(float_256_vec_0, float_256_vec_1, float_256_vec_2);
+
+    flo = (float*) &float_256_result;
+    printf("float:\t\t%f, %f, %f, %f, %f, %f, %f, %f\n", flo[0], flo[1], flo[2], flo[3], flo[4], flo[5], flo[6], flo[7]);
+
+    // Double-precision multiply negated and add with 256-bit vectors (FMA)
+    __m256d double_256_vec_0 = _mm256_set1_pd(8.0);
+    __m256d double_256_vec_1 = _mm256_set1_pd(20.0);
+    __m256d double_256_vec_2 = _mm256_set1_pd(2.0);
+
+    __m256d double_256_result = _mm256_fnmsub_pd(double_256_vec_0, double_256_vec_1, double_256_vec_2);
+
+    dou = (double*) &double_256_result;
+    printf("double:\t\t%lf, %lf, %lf, %lf\n", dou[0], dou[1], dou[2], dou[3]);
+
+    // Single-precision multiply negated and add the lowest element with 128-bit vectors (FMA)
+    __m128 float_128_low_result = _mm_fnmsub_ss(float_128_vec_0, float_128_vec_1, float_128_vec_2);
+
+    flo = (float*) &float_128_low_result;
+    printf("float:\t\t%f, %f, %f, %f\n", flo[0], flo[1], flo[2], flo[3]);
+
+    // Double-precision multiply negated and add the lowest element with 128-bit vectors (FMA)
+    __m128d double_128_low_result = _mm_fnmsub_sd(double_128_vec_0, double_128_vec_1, double_128_vec_2);
+
+    dou = (double*) &double_128_low_result;
+    printf("double:\t\t%lf, %lf\n", dou[0], dou[1]);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -206,7 +206,21 @@ Then you would find out all the output files are gone away!
     > - [_mm_fmsub_ss](Arithmetic_Intrinsics/src/fmsub.c#L52)       `FMA`
     > - [_mm_fmsub_sd](Arithmetic_Intrinsics/src/fmsub.c#L58)       `FMA`
 
+- [fnmadd](Arithmetic_Intrinsics/src/fnmadd.c)
+    > - [_mm_fnmadd_ps](Arithmetic_Intrinsics/src/fnmadd.c#L16)       `FMA`
+    > - [_mm_fnmadd_pd](Arithmetic_Intrinsics/src/fnmadd.c#L26)       `FMA`
+    > - [_mm256_fnmadd_ps](Arithmetic_Intrinsics/src/fnmadd.c#L36)    `FMA`
+    > - [_mm256_fnmadd_ps](Arithmetic_Intrinsics/src/fnmadd.c#L46)    `FMA`
+    > - [_mm_fnmadd_ss](Arithmetic_Intrinsics/src/fnmadd.c#L52)       `FMA`
+    > - [_mm_fnmadd_sd](Arithmetic_Intrinsics/src/fnmadd.c#L58)       `FMA`
 
+- [fnmsub](Arithmetic_Intrinsics/src/fnmsub.c)
+    > - [_mm_fnmsub_ps](Arithmetic_Intrinsics/src/fnmsub.c#L16)       `FMA`
+    > - [_mm_fnmsub_pd](Arithmetic_Intrinsics/src/fnmsub.c#L26)       `FMA`
+    > - [_mm256_fnmsub_ps](Arithmetic_Intrinsics/src/fnmsub.c#L36)    `FMA`
+    > - [_mm256_fnmsub_ps](Arithmetic_Intrinsics/src/fnmsub.c#L46)    `FMA`
+    > - [_mm_fnmsub_ss](Arithmetic_Intrinsics/src/fnmsub.c#L52)       `FMA`
+    > - [_mm_fnmsub_sd](Arithmetic_Intrinsics/src/fnmsub.c#L58)       `FMA`
 
 ## Copyright
 
